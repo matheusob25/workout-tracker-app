709fe287994c2938fe04ee2f0ec9f2222405d508
matheusob25 <matheusoliveira0890@gmail.com>
2026-03-29 00:28:31 -0300

    fixing prisma service config

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from 'generated/prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient  {
 
     constructor(){
         const adapter = new PrismaPg({
-            url: process.env.DATABASE_URL
+            connectionString: process.env.DATABASE_URL as string,
         });
-        super({adapter});
+        super({adapter}); 
 
     }
     
